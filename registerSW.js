514c5c38765e6a052728dd04615abfd0d7e03576
Jessica Kuang <74442544+jessica-kuang@users.noreply.github.com>
2024-04-25 15:06:02 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/5440-pwa/sw.js', { scope: '/5440-pwa/' })})}
